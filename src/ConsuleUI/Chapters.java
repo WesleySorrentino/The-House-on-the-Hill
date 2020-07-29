04aa43d560cefca94d7c156f967466d4c53f1812
Wesley Sorrentino <wes.james17@gmail.com>
2020-07-28 23:35:35 -0400

    Changed Dialogue to Chapters, and added Intro to game

---
+++ src/ConsuleUI/Chapters.java
@@ -0,0 +1,30 @@
+package ConsuleUI;
+
+import Utilities.Utilities;
+
+public class Chapters {
+    public static void intro() {
+        String introText =
+                "Intro: " +
+                        "September 13th, It’s the beginning of the school year. Everyday on your way to school every year,\n" +
+                        "You pass by this house on the hill. It has been deteriorating and has been covered with vines for several years.\n" +
+                        "Once you get to school you tell your best friend.\n" +
+
+                        "\nJake: Hey Mark, We should go explore the house on the hill after school\n" +
+                        "\nMark: Have you been reading your dads hardy books again?\n" +
+                        "\nJake: Yes, but that’s not the point, According to this newspaper (pulls out newspaper)\n" +
+                        "Date is 1990, It says the owners got up and left, leaving everything behind. No one knows Why they left.\n" +
+                        "\nMark: Do you know how dangerous it is? There could be ghosts!\n" +
+                        "\nJake: Ghosts don't exist Mark. Have you ever watched Scooby doo?\n" +
+                        "\nThere is always a person behind the monster or ghost.\n" +
+                        "\nMark: Of course I watch Scooby doo you numb nut, let me think about it\n" +
+                        "\nNarrator: Mark walks to class and follows\n" +
+                        "\nNarrator: The time is now 4:00..\n" +
+                        "\nJake: What do you say Mark?\n" +
+                        "\nMark: You know what Jake, I'm in.\n" +
+                        "\nJake: Awesome, we will go on Saturday. We can tell our parent we are just going to hangout in town.\n" +
+                        "\nNarrator: The day is now Saturday the boys pack the supplies they might need for the exploration, with them being Boy Scouts they know to Be Prepared.\n";
+
+        Utilities.slowTextScroll(introText,1100);
+    }
+}
