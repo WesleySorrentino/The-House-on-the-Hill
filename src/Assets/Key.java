43d2a156af413adc052f8958da0ec7c3cbb0a382
Wesley Sorrentino <wes.james17@gmail.com>
2020-07-31 10:18:01 -0400

    Added Funcionallity to Player

--- src/Assets/Key.java
+++ src/Assets/Key.java
@@ -2,19 +2,34 @@ package Assets;
 
 // Handles all the different keys the user can get
 public class Key extends Item{
-    private int id;
-
+    private final int id;
+    private final int quantity;
     /**
-     *
      * @param name Enter the name of the key
      * @param id Enter the specific Identifier for key
+     * @param quantity enter the quantity
      */
-    public Key(String name, int id) {
+    public Key(String name, int id, int quantity) {
         super(name);
         this.id = id;
+        this.quantity = quantity;
+    }
+
+    /**
+     * @param name Enter the name of the key
+     * @param id Enter the specific Identifier for key
+     *
+     * Quantity is default to 1
+     */
+    public Key(String name, int id) {
+        this(name,id,1);
     }
 
     public int getId() {
         return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
--- src/Assets/Player.java
+++ src/Assets/Player.java
@@ -1,25 +1,65 @@
 package Assets;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Player {
     private final String name;
-    private final Map<Item,Integer> inventory = new HashMap<>();
+    private final Map<Key, Integer> inventory = new HashMap<>();
+    private final ArrayList<Key> keyInventory = new ArrayList<>();
+    private final Scanner s = new Scanner(System.in);
 
     public Player(String name) {
         this.name = name;
-
-        inventory.put(new Item("Flashlight"),2);
-        inventory.put(new Item("Pocket Knife"),1);
-        inventory.put(new Item("Water"),1);
     }
 
-    public void displayInventory() {
-        for (Map.Entry<Item,Integer> entry : inventory.entrySet()) {
-            Item items = entry.getKey();
-            int quantity = entry.getValue();
-            System.out.println(items.getName() + ": x" + quantity);
+    private void displayInventory() {
+        int increment = 1;
+        for (Key key : keyInventory) {
+            System.out.println(increment++ + ". " + key.getName() + " x" + key.getQuantity());
         }
+    }
+
+    public void useKey(Door door) {
+        displayInventory();
+        System.out.println("Type the number of the key you want to use: ");
+        int choice = s.nextInt();
+
+        if (choice <= keyInventory.size()) {
+            door.openDoor(keyInventory.get(choice - 1));
+            keyInventory.remove(choice -1);
+        } else {
+            System.out.println("Please enter a valid number!");
+        }
+    }
+    /**
+     * @param item enter the item you wanna give the user
+     */
+    public void addToInventory(Key item) {
+        keyInventory.add(item);
+        System.out.println("Added " + item.getName() + " to inventory.\n");
+    }
+//  Display's the available options for the user
+    public void userAction(Door door) {
+        System.out.println("1. Use Key");
+        System.out.println("2. Display Inventory");
+
+        System.out.println("Type a number listed above: ");
+        int selection = s.nextInt();
+        s.nextLine();
+
+        switch (selection) {
+            case 1:
+                useKey(door);
+                break;
+            case 2:
+                displayInventory();
+                break;
+            default:
+                System.out.println("Please enter a valid number!");
+        }
+    }
+
+    public String getName() {
+        return name;
     }
 }
